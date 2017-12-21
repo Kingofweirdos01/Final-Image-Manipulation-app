f820cfa7389e8a969ceae7caa8aa2f6ed5395a7f
Kingofweirdos01 <32146052+Kingofweirdos01@users.noreply.github.com>
2017-12-20 23:03:32 -0800

    Add files via upload
    The /ImageManipulation-p5/ImageManipulation.sln is the actual app the rest is all the objects and code which you need

---
+++ ImageManipulation-p5/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ImageManipulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void openImage()
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+                opened = true;
+            }
+        }
+        void hue()
+        {
+            float changered = trackBar2.Value * 0.1f;
+            float changeyellow = trackBar1.Value * 0.1f;
+            float changeblue = trackBar1.Value * 0.1f;
+
+           
+
+            reload();
+            if (!opened)
+            {
+            }
+            else
+            {
+
+
+
+                Image img = pictureBox1.Image;                             // storing image into img variable of image type from picturebox1
+                Bitmap bmpInverted = new Bitmap(img.Width, img.Height);   /* creating a bitmap of the height of imported picture in picturebox which consists of the pixel data for a graphics image
+                                                                        and its attributes. A Bitmap is an object used to work with images defined by pixel data.*/
+
+                ImageAttributes ia = new ImageAttributes();                 //creating an object of imageattribute ia to change the attribute of images
+                ColorMatrix cmPicture = new ColorMatrix(new float[][]       // now creating the color matrix object to change the colors or apply  image filter on image
+                {
+                    new float[]{1+changered, 0, 0, 0, 0},
+            new float[]{0, 1+changeyellow, 0, 0, 0},
+            new float[]{0, 0, 1+changeblue, 0, 0},
+            new float[]{0, 0, 0, 1, 0},
+            new float[]{0, 0, 0, 0, 1}
+                });
+                ia.SetColorMatrix(cmPicture);           //pass the color matrix to imageattribute object ia
+                Graphics g = Graphics.FromImage(bmpInverted);   /*create a new object of graphics named g, ; Create graphics object for alteration.
+                                                            Graphics newGraphics = Graphics.FromImage(imageFile); is the format of loading image into graphics for alteration*/
+
+                g.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+
+                /*   g.drawimage(image, new rectangle(location of rectangle axix-x, location axis-y, width of rectangle, height of rectangle),
+               location of image in rectangle x-axis, location of image in rectangle y-axis, width of image, height of image,
+               format of graphics unit,provide the image attributes   */
+
+
+                g.Dispose();                            //Releases all resources used by this Graphics.
+                pictureBox1.Image = bmpInverted;
+
+
+            }
+        }
+
+
+        void reload()
+        {
+            if (!opened)
+            {
+                // MessageBox.Show("Open an Image then apply changes");
+            }
+            else
+            {
+                if (opened)
+                {
+                    file = Image.FromFile(openFileDialog1.FileName);
+                    pictureBox1.Image = file;
+                    opened = true;
+                }
+            }
+        }
+
+
+
+        void saveImage()
+        {
+            if (opened)
+            {
+                SaveFileDialog sfd = new SaveFileDialog(); // create a new save file dialog object
+                sfd.Filter = "Images|*.png;*.bmp;*.jpg";
+                ImageFormat format = ImageFormat.Png;// you want to store it in by default format
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string ext = Path.GetExtension(sfd.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    pictureBox1.Image.Save(sfd.FileName, format);
+                }
+
+
+
+            }
+            else { MessageBox.Show("No image loaded, first upload image "); }
+
+        }
+
+
+        Image file;
+        Boolean opened = false; // to check whether the image is open in the picture box or not
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            saveImage();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            openImage();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            hue();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            hue();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            hue();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
